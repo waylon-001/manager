02662a2555abfcdd515fb1454ff7d9762af405c3
jswaylon <jswaylon@itcom888.com>
2019-11-21 18:41:37 +0800

    线上修改

---
+++ backend/src/main/java/cc/mrbird/febs/Application.java
@@ -0,0 +1,30 @@
+package cc.mrbird.febs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+@EnableScheduling
+@EnableAsync
+public class Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+/*    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class)
+                .run(args);
+    }*/
+}
